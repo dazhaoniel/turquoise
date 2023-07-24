c04df2a7cf1bfb2c9b0ab9bd80a2e279e9d542d0
Daniel Zhao <dazhaoniel@users.noreply.github.com>
2023-07-24 04:10:45 +0000

    update makefile

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+
+def read_images():
+    '''Read images'''
+    pass
